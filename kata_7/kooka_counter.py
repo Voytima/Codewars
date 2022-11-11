9ddbf1592ab35f4c2e4ed7866e787fba6b973149
Dmitry Voytik <voytima@gmail.com>
2022-11-11 15:00:20 +0300

    Kooka-Counte

---
+++ kata_7/kooka_counter.py
@@ -0,0 +1,13 @@
+def kooka_counter(laughing):
+    if len(laughing) == 0:
+        return 0
+
+    count = 1
+    laughing = laughing.replace('a', '')
+
+    for i in range(len(laughing) - 1):
+        if laughing[i] != laughing[i + 1]:
+            count += 1
+        i += 1
+
+    return count

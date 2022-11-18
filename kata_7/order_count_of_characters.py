77561d8e2647193b154393b5f6efdbca47d667a5
Dmitry Voytik <voytima@gmail.com>
2022-11-18 12:42:14 +0300

    Ordered Count of Characters

---
+++ kata_7/order_count_of_characters.py
@@ -0,0 +1,21 @@
+"""
+Count the number of occurrences of each character and return it as a (list of tuples) in order of appearance.
+For empty output return (an empty list).
+Consult the solution set-up for the exact data structure implementation depending on your language.
+
+Example:
+ordered_count("abracadabra") == [('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1)]
+"""
+
+
+def ordered_count(inp):
+    counts = []
+    for i in inp:
+        if i not in counts:
+            counts.append(i)
+    return [(i, inp.count(i)) for i in counts]
+
+
+print(ordered_count('abracadabra'))
+
+
